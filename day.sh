7ae54a51ee947e2f35120266909ca7ab9005146f
Jari Hast <jari.hast@haahtela.fi>
2022-12-02 11:42:26 +0200

    Day 1

---
+++ day.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euxo pipefail
+
+mkdir "day$1"
+
+pushd "day$1"
+
+dotnet new console -lang "F#" 
+
+dotnet new tool-manifest
+dotnet tool install --local fantomas-tool
+
+touch test.txt
+touch input.txt
+
+mkdir .vscode
+popd
+
+cp vscode-settings.json "day$1"/.vscode/settings.json
---
+++ day01/Program.fs
@@ -0,0 +1,29 @@
+﻿open System.IO
+
+let input = File.ReadAllLines("./input.txt") |> Array.toList
+
+let splat =
+    input
+    |> List.fold
+        (fun grouped line ->
+            match line with
+            | "" -> [] :: grouped
+            | _ -> (grouped.Head @ [ (int line) ]) :: grouped.Tail
+
+            )
+        [ [] ]
+
+let sums = splat |> List.map List.sum
+
+let res1 = sums |> List.max
+
+printfn $"%A{res1}"
+
+let res2 =
+    sums
+    |> List.sort
+    |> List.rev
+    |> List.take 3
+    |> List.sum
+
+printfn $"%A{res2}"
