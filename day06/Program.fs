11132e0a2183b289886dc1a8e85a40504493175c
Jari Hast <jari.hast@skillgrower.com>
2022-12-06 16:40:52 +0200

    day 6

---
+++ day06/Program.fs
@@ -0,0 +1,23 @@
+﻿open System.IO
+open System
+
+let input =
+    File.ReadAllLines("./input.txt")
+    |> Array.toList
+    |> List.head
+
+let chars = input |> List.ofSeq |> List.map string
+
+let solve win =
+    chars
+    |> List.windowed win
+    |> List.findIndex (fun ls -> ls |> set |> Set.count = win)
+    |> (+) win
+
+let res1 = solve 4
+
+printfn $"%A{res1}"
+
+let res2 = solve 14
+
+printfn $"%A{res2}"
