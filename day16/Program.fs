8eb103aeb962769ea7373da400475a8d78334635
Jari Hast <jari.hast@skillgrower.com>
2022-12-25 14:26:04 +0200

    day 16

--- day16/Program.fs
+++ day16/Program.fs
@@ -1,8 +1,7 @@
 open System.IO
 open System.Text.RegularExpressions
 
-
-let input = File.ReadAllLines("./test.txt") |> Array.toList
+let input = File.ReadAllLines("./input.txt") |> Array.toList
 
 let triplets =
     input
@@ -32,6 +31,7 @@ let rec travel visited total cur =
             notVisited
             |> List.map (fun next -> travel (cur :: visited) (total + 1) next)
             |> List.concat
+            |> List.append acc
 
     visits
     |> List.groupBy fst
@@ -41,36 +41,69 @@ let rec travel visited total cur =
 let graph =
     triplets
     |> Map.map (fun k (m, _) -> m, travel [] 0 k)
-
+    |> Map.filter (fun k (m, _) -> m > 0 || k = "AA")
+    |> Map.map (fun k (m, vs) -> m, vs |> List.filter (fun (dest, _) -> dest <> k))
 
 let rec traverse total steps maxSteps visited cur =
     let (rate, nexts) = Map.find cur graph
 
     let notVisited =
-        nexts
-        |> set
-        |> fun s -> Set.difference s visited
+        List.filter (fun (dest, _) -> not (List.contains dest visited)) nexts
 
-    if steps = maxSteps || notVisited.Count = 0 then
-        total
+    let addedRate = rate * (max (maxSteps - steps) 0)
+
+    if steps >= maxSteps || (List.isEmpty notVisited) then
+        total + addedRate
     else
-        let noOpen =
+        let addedRate = rate * (maxSteps - steps)
+        let openCost = if cur = "AA" then 0 else 1
+
+        let openValve =
             notVisited
-            |> Set.map (traverse total (steps + 1) maxSteps (Set.add cur visited))
-            |> Set.maxElement
+            |> List.map (fun (n, s) -> traverse (total + addedRate) (steps + s + openCost) maxSteps (cur :: visited) n)
+            |> List.max
 
-        if rate > 0 then
-            let addedRate = rate * (maxSteps - steps + 1)
+        openValve
 
-            let openValve =
-                notVisited
-                |> Set.map (traverse (total + addedRate) (steps + 2) maxSteps (Set.add cur visited))
-                |> Set.maxElement
-
-            max openValve noOpen
-        else
-            noOpen
-
-let res1 = traverse 0 1 30 Set.empty "AA"
+let res1 = traverse 0 1 30 [] "AA"
 
 printfn "%A" res1
+
+let rec comb n l =
+    match (n, l) with
+    | (0, _) -> [ [] ]
+    | (_, []) -> []
+    | (n, x :: xs) ->
+        let useX = List.map (fun l -> x :: l) (comb (n - 1) xs)
+        let noX = comb n xs
+        useX @ noX
+
+let dests =
+    Map.find "AA" graph
+    |> fun (_, ls) -> List.map fst ls
+
+let destlen = List.length dests
+
+let splits = [ 3 .. (destlen / 2) ]
+
+let all =
+    splits
+    |> List.map (fun n -> comb n dests)
+    |> List.concat
+    |> List.map (fun ls -> (ls, List.except ls dests))
+
+let calc one other =
+    async {
+        let first = traverse 0 1 26 one "AA"
+        let second = traverse 0 1 26 other "AA"
+        return first + second
+    }
+
+let res2 =
+    all
+    |> List.map (fun (one, other) -> calc one other)
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Array.max
+
+printfn "%A" res2
